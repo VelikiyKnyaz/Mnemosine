d061b9bf5d6881978f802fb2c8c55fcb101fde55
Tobias <tobiasdelvalledelviento@gmail.com>
2026-07-26 01:20:42 -0700

    fix: expose Snack-compatible default entry

--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ import App from './App';
 import { registerRootComponent } from 'expo';
 
 registerRootComponent(App);
+
+// Snack's web editor also discovers the root through a default export.
+export default App;
